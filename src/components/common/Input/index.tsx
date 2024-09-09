56e320af43a99fe3e72d4b5d78c69ae427c2ccc9
Choyoungjun <cyj070513@gmail.com>
2024-09-09 19:27:25 +0900

    feat :: 컴포넌트

---
+++ src/components/common/Input/index.tsx
@@ -0,0 +1,113 @@
+import { theme } from "../../../styles/theme";
+import { useState } from "react";
+import { styled } from "styled-components";
+import EyeOff from "@/assets/svg/eye-off.svg";
+import EyeOpen from "@/assets/svg/eye.svg";
+
+interface InputProp {
+  label?: string;
+  type: "text" | "password" | "textarea";
+  placeholder?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  name?: string;
+  value?: string;
+  onKeyDown?: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+}
+
+const Input = ({
+  label,
+  type,
+  placeholder,
+  onChange,
+  name,
+  value,
+  onKeyDown,
+}: InputProp) => {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  return (
+    <Container>
+      <InputLabel>{label}</InputLabel>
+      <InputContainer>
+        <InputContent
+          type={
+            type === "password" ? (showPassword ? "text" : "password") : type
+          }
+          placeholder={placeholder}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+          name={name}
+          value={value}
+          autoComplete="off"
+        />
+        {type === "password" && (
+          <EyesContainer onClick={() => setShowPassword(!showPassword)}>
+            {showPassword ? (
+              <EyeImg src={EyeOpen} alt="" />
+            ) : (
+              <EyeImg src={EyeOff} alt="" />
+            )}
+          </EyesContainer>
+        )}
+      </InputContainer>
+    </Container>
+  );
+};
+
+export default Input;
+
+const InputContainer = styled.div`
+  display: flex;
+  width: 600px;
+  border-radius: 8px;
+  padding: 11px 16px;
+  background-color: ${theme.color.gray[900]};
+  border: 1px solid ${theme.color.gray[900]};
+
+  &:hover {
+    border: 1px solid ${theme.color.main[500]};
+  }
+
+  &:focus-within {
+    border: 1px solid ${theme.color.main[500]};
+  }
+`;
+
+const InputContent = styled.input`
+  display: flex;
+  height: 24px;
+  width: 100%;
+
+  background-color: ${theme.color.gray[900]};
+  color: ${theme.color.white};
+  outline: none;
+
+  caret-color: ${theme.color.main[500]};
+
+  &::placeholder {
+    font-size: ${theme.font.caption[1].size};
+    font-weight: ${theme.font.caption[1].fontweight};
+    color: ${theme.color.gray[600]};
+  }
+`;
+
+const InputLabel = styled.label`
+  font-size: ${theme.font.label[1].size};
+  font-weight: ${theme.font.label[1].fontweight};
+  color: ${theme.color.white};
+`;
+
+const EyesContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+const EyeImg = styled.img`
+  background-color: ${theme.color.gray[900]};
+`
