ff341d333f086ef7e8c4568e2b95dbe5c7188f29
hyuna <hyuna_0525@dsm.hs.kr>
2024-09-12 19:38:19 +0900

    add :: svg 추가

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -5,6 +5,8 @@ export { default as BasicProfile } from "./basicProfile.svg";
 export { default as AssignmentIcon } from "./assignment";
 export { default as SubmitIcon } from "./submit.svg";
 export { default as DeleteIcon } from "./delete.svg";
+export { default as ProfileChangeIcon } from "./profileChange.svg";
+export { default as LogOutIcon } from "./logout.svg";
 
 export interface IconProp {
   Fill: string;
