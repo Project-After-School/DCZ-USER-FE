3441ce980e7250f2ebb2de52a7559c4b73147988
Choyoungjun <cyj070513@gmail.com>
2024-09-13 08:49:13 +0900

    chore :: 정리

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,42 +1,26 @@
-import Input from '@/components/common/Input';
-import Button from '@/components/common/Button';
+import Input from "@/components/common/Input";
+import Button from "@/components/common/Button";
 import { styled } from "styled-components";
-import { DCZLogo } from '@/assets/svg';
+import { DCZLogo } from "@/assets/svg";
 import { theme } from "@/styles/theme";
 
 const Login = () => {
   return (
     <Container>
-        <img src={DCZLogo}/>
-        <SubContainer>
+      <img src={DCZLogo} />
+      <SubContainer>
         <InputContainer>
-        <TextContainer>
-            <LoginText>
-                로그인 하기
-            </LoginText>
-            <SubText>
-                스퀘어 계정으로 로그인해주세요
-            </SubText>
-        </TextContainer>
-            <Input
-                type='text'
-                label='아이디'
-                placeholder='아이디 입력'
-            />
-            <Input
-                type='password'
-                label='비밀번호'
-                placeholder='비밀번호 입력'
-            />
+          <TextContainer>
+            <LoginText>로그인 하기</LoginText>
+            <SubText>스퀘어 계정으로 로그인해주세요</SubText>
+          </TextContainer>
+          <Input type="text" label="아이디" placeholder="아이디 입력" />
+          <Input type="password" label="비밀번호" placeholder="비밀번호 입력" />
         </InputContainer>
-        <Button
-            width='318px'
-        >
-        로그인하기
-        </Button>
-        </SubContainer>
+        <Button width="318px">로그인하기</Button>
+      </SubContainer>
     </Container>
-  )
+  );
 };
 
 export default Login;
@@ -59,32 +43,31 @@ const SubContainer = styled.div`
 `;
 
 const TextContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
 `;
 
 const InputContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 60px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 60px;
 `;
 
 const LoginText = styled.p`
-     font-size: ${theme.font.heading[2].size};
-     font-weight: ${theme.font.heading[2].fontweight};
-    color: ${theme.color.white};
-    user-select: none;
+  font-size: ${theme.font.heading[2].size};
+  font-weight: ${theme.font.heading[2].fontweight};
+  color: ${theme.color.white};
+  user-select: none;
 `;
 
 const SubText = styled.p`
-    font-size: ${theme.font.label[1].size};
-    font-weight: ${theme.font.label[1].fontweight};
-    color: ${theme.color.gray[200]};
-    user-select: none;
+  font-size: ${theme.font.label[1].size};
+  font-weight: ${theme.font.label[1].fontweight};
+  color: ${theme.color.gray[200]};
+  user-select: none;
 `;
-
